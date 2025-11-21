c46278358b3aca65b9147d1ae7e95eda2f5fe71c
u1e2k <118117293+u1e2k@users.noreply.github.com>
2025-11-21 19:10:10 +0000

    first

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # php-minecraft-sns-connect
 A repository to preserve memories of scripts I made long ago
+
+> composer使えるのか。
+
+## require
+
+`gh repo clone xPaw/PHP-Minecraft-Query`
+
+## 記憶の内容
+PHP rcon と PHP Query を使って、今はもう使えないけどTwitterの認証をして、  
+MySQLから該当するアカウント名とMinecraftのユーザー名が合致していたら、  
+データベースにあるMinecraft内の通貨Pluginの値を返す。  
+管理者だった場合はRCONのコンソールを用意して遠隔で操作ができるようにというところまでは作ってたはず。  
+不運にも当時GitHubを知らなくてサーバーのデータ消えたから何も残ってない。
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f53b38a93485b4a4fbbae27b5d770e8288cb9ff',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f53b38a93485b4a4fbbae27b5d770e8288cb9ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'thedudeguy/rcon' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ada4eb08304dfe9562192978444fbfc21a08a9ca',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../thedudeguy/rcon',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
